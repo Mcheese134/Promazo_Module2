1b71bef755f951b1a7d8e0cf0d5e6f64062b3881
Someone <playground@ip-172-31-86-81.ec2.internal>
2020-02-11 16:36:59 +0000

    Add Requirements.txt

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:quiz_id>/', views.detail, name = 'detail'),
+    path('<int:quiz_id>/quiz_list/', views.quiz_list, name='quiz_list'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+#from .module import Quiz
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+   # latest_quiz_list = Quiz.objects.order_by('pub_date')[:5]
+   # output = ', '.join([q.quiz_text for q in latest_quiz_list]) 
+    return HttpResponse("Hello This should page should contain a list of quizzes.")
+
+#This will display the different quizzes made in the admin screen
+def quiz_list(request,quiz_id): 
+    return HttpResponse("This is the list of quizzes for %s" % quiz_id)
+
+#This is the details view for a specified quiz
+def detail(request, quiz_id):
+    return HttpResponse("You're looking at quiz %s." % quiz_id)
+
 
 # Create your views here.
